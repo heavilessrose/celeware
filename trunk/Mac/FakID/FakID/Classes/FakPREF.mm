72ff5dcb5522e865caf713836c9518e9a4339caf
YonsmGuo@gmail.com <YonsmGuo@gmail.com@841263ce-9077-5313-03d4-c3799327427c>
2012-09-29 00:20:03 +0000

    3.1

--- trunk/Mac/FakID/FakID/Classes/FakPREF.mm
+++ trunk/Mac/FakID/FakID/Classes/FakPREF.mm
@@ -6,63 +6,13 @@
 
 
 //
-NSInteger _orginRowsInSection1;
-NSInteger _totalRowsInSection1;
-static IMP pNumberOfRowsInSection;
-NSInteger MyNumberOfRowsInSection(id<UITableViewDataSource> self, SEL cmd, UITableView *tableView, NSInteger section)
-{
-	NSInteger ret = (NSInteger)pNumberOfRowsInSection(self, cmd, tableView, section);
-	if (section == 1)
-	{
-		_orginRowsInSection1 = ret;
-		if (ret < 14) ret += 2;
-		_totalRowsInSection1 = ret;
-		_Log(@"MyNumberOfRowsInSection: _orginRowsInSection1=%d, _totalRowsInSection1=%d", _orginRowsInSection1, _totalRowsInSection1);
-	}
-	return ret;
-}
-
-
-//
 static IMP pCellForRowAtIndexPath;
 UITableViewCell *MyCellForRowAtIndexPath(id<UITableViewDataSource> self, SEL cmd, UITableView *tableView, NSIndexPath *indexPath)
 {
 	@autoreleasepool
 	{
-		UITableViewCell *cell;
-		
-		if ((indexPath.section != 1) || (indexPath.row < _orginRowsInSection1))
-		{
-			cell = pCellForRowAtIndexPath(self, cmd, tableView, indexPath);
-		}
-		else
-		{
-			NSString *reuse = @"FakPREFCell";
-			cell = [tableView dequeueReusableCellWithIdentifier:reuse];
-			if (cell == nil)
-			{
-				cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuse];// autorelease];
-				cell.selectionStyle = UITableViewCellSelectionStyleNone;
-				//cell = pCellForRowAtIndexPath(self, cmd, tableView, [NSIndexPath indexPathForRow:1 inSection:_orginRowsInSection1 - 1]);
-			}
-		}
-		
-		if (indexPath.section == 1)
-		{
-			if (indexPath.row == _totalRowsInSection1 - 1)
-			{
-				cell.textLabel.text = NSLocalizedString(@"ModemVersion", @"Modem Firmware");
-			}
-			else if (indexPath.row == _totalRowsInSection1 - 2)
-			{
-				cell.textLabel.text = NSLocalizedString(@"ICCID", @"ICCID");
-			}
-			else if (indexPath.row == _totalRowsInSection1 - 3)
-			{
-				cell.textLabel.text = NSLocalizedString(@"ModemIMEI", @"IMEI");
-			}
-		}
-		
+		UITableViewCell *cell = pCellForRowAtIndexPath(self, cmd, tableView, indexPath);
+
 		_Log(@"FakPREF Name: %@, Value: %@", cell.textLabel.text, cell.detailTextLabel.text);
 		for (NSString *key in ITEMS().allKeys)
 		{
@@ -84,30 +34,22 @@ UITableViewCell *MyCellForRowAtIndexPath(id<UITableViewDataSource> self, SEL cmd
 			}
 		}
 		
-		return cell;
-	}
-}
-
-//
-static IMP pCellForRowAtIndexPathForGeneral;
-UITableViewCell *MyCellForRowAtIndexPathForGeneral(id<UITableViewDataSource> self, SEL cmd, UITableView *tableView, NSIndexPath *indexPath)
-{
-	@autoreleasepool
-	{
-		
-		UITableViewCell *cell = pCellForRowAtIndexPathForGeneral(self, cmd, tableView, indexPath);
-		if ((indexPath.section == 1) && (indexPath.row == 1) && (cell.accessoryType == UITableViewCellAccessoryNone))
+		//
+		if ([cell.textLabel.text hasPrefix:@"Wi-Fi"])
 		{
-			cell.textLabel.enabled = YES;
-			cell.detailTextLabel.enabled = YES;
-			cell.textLabel.textColor = UIColor.blackColor;
-			cell.detailTextLabel.textColor = [UIColor colorWithRed:0.22 green:0.33 blue:0.53 alpha:1];
-			_Log(@"FakPREF MyCellForRowAtIndexPathForGeneral: %@", cell.detailTextLabel.text);
-			cell.detailTextLabel.text = NSLocalizedString(@"Off", @"Off");
-			cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-			cell.selectionStyle = UITableViewCellSelectionStyleBlue;
+			if ([[ITEMS() objectForKey:@"RegionInfo"] isEqualToString:@"CH/A"])
+			{
+				if ([NSUtil::DefaultLanguage() hasPrefix:@"en"])
+				{
+					cell.textLabel.text = [cell.textLabel.text stringByReplacingOccurrencesOfString:@"Wi-Fi" withString:@"WLAN"];
+				}
+				else if ([NSUtil::DefaultLanguage() hasPrefix:@"zh"])
+				{
+					cell.textLabel.text = [cell.textLabel.text stringByReplacingOccurrencesOfString:@"Wi-Fi " withString:@"无线局域网"];
+				}
+			}
 		}
-		
+
 		return cell;
 	}
 }
@@ -122,21 +64,11 @@ UITableViewCell *MyCellForRowAtIndexPathForRoot(id<UITableViewDataSource> self, 
 		_Log(@"\n\nFakPREF MyCellForRowAtIndexPathForRoot %d", 1212);
 		
 		UITableViewCell *cell = pCellForRowAtIndexPathForRoot(self, cmd, tableView, indexPath);
-		if ((indexPath.section == 0) && (indexPath.row == 1))
+
+		_LogObj(cell.textLabel.text);
+		if ([cell.textLabel.text isEqualToString:@"Wi-Fi"])
 		{
-			if (cell.accessoryType == UITableViewCellAccessoryNone)
-			{
-				cell.textLabel.enabled = YES;
-				cell.detailTextLabel.enabled = YES;
-				cell.textLabel.textColor = UIColor.blackColor;
-				cell.detailTextLabel.textColor = [UIColor colorWithRed:0.22 green:0.33 blue:0.53 alpha:1];
-				_Log(@"FakPREF MyCellForRowAtIndexPathForGeneral: %@", cell.detailTextLabel.text);
-				cell.detailTextLabel.text = NSLocalizedString(@"Off", @"Off");
-				cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-				cell.selectionStyle = UITableViewCellSelectionStyleBlue;
-			}
-			_LogObj(cell.textLabel.text);
-			if ([cell.textLabel.text isEqualToString:@"Wi-Fi"])
+			if ([[ITEMS() objectForKey:@"RegionInfo"] isEqualToString:@"CH/A"])
 			{
 				if ([NSUtil::DefaultLanguage() hasPrefix:@"en"])
 				{
@@ -166,7 +98,6 @@ void MyDidSelectRowAtIndexPathForReset(id<UITableViewDataSource> self, SEL cmd, 
 {
 	@autoreleasepool
 	{
-		
 		_Log(@"\n\nFakPREF MyDidSelectRowAtIndexPathForReset %d", 1213);
 		
 		if ((indexPath.row == 1) && (indexPath.section == 0))
@@ -174,7 +105,6 @@ void MyDidSelectRowAtIndexPathForReset(id<UITableViewDataSource> self, SEL cmd, 
 			indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
 		}
 		pDidSelectRowAtIndexPathForReset(self, cmd, tableView, indexPath);
-		
 	}
 }
 
@@ -190,11 +120,7 @@ extern "C" void FakPREFInitialize()
 			_LogLine();
 			
 			MSHookMessageEx(NSClassFromString(@"AboutController"), @selector(tableView: cellForRowAtIndexPath:), (IMP)MyCellForRowAtIndexPath, (IMP *)&pCellForRowAtIndexPath);
-			MSHookMessageEx(NSClassFromString(@"AboutController"), @selector(tableView: numberOfRowsInSection:), (IMP)MyNumberOfRowsInSection, (IMP *)&pNumberOfRowsInSection);
-			
-			MSHookMessageEx(NSClassFromString(@"GeneralController"), @selector(tableView: cellForRowAtIndexPath:), (IMP)MyCellForRowAtIndexPathForGeneral, (IMP *)&pCellForRowAtIndexPathForGeneral);
 			MSHookMessageEx(NSClassFromString(@"PrefsListController"), @selector(tableView: cellForRowAtIndexPath:), (IMP)MyCellForRowAtIndexPathForRoot, (IMP *)&pCellForRowAtIndexPathForRoot);
-			
 			MSHookMessageEx(NSClassFromString(@"ResetPrefController"), @selector(tableView: didSelectRowAtIndexPath:), (IMP)MyDidSelectRowAtIndexPathForReset, (IMP *)&pDidSelectRowAtIndexPathForReset);
 		}
 	}
